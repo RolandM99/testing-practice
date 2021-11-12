7b4ff65ddc7174d8a745e2c85a953dafa87e3c5b
rolandm99 <manfulmweze99@gmail.com>
2021-11-12 08:23:19 +0100

    Remove parenthesis from return in Calculator

--- calculator.js
+++ calculator.js
@@ -1,18 +1,18 @@
 class Calculator {
   static add (a, b) {
-    return (a + b);
+    return a + b;
   }
 
   static substract (a, b) {
-    return (a - b);
+    return a - b;
   }
 
   static divide (a, b) {
-    return (a / b);
+    return a / b;
   }
 
   static multiply (a, b) {
-    return (a * b);
+    return a * b;
   }
 }
 
